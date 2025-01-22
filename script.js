c0643c4b16bc8421c319257d0aad50ffe3495b50
BRIGHTGAINSCOMPANY <162323582+BRIGHTGAINSCOMPANY@users.noreply.github.com>
2025-01-22 22:00:35 +0300

    Global objects

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const name = 'Eric';
-
-// console.log(name);
-
-//global objects
-console.log(global);
-
-global.setTimeout(() => {
-    console.log("in the timeout");
-
-}, 3000);
+const name = 'Eric';
+console.log(name);
 
